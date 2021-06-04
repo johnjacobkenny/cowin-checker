afbad0b413dcbd51a21687f243699dfb3e1eed32
Kenny John Jacob <kenny.jacob@mattermotor.com>
2021-06-04 21:57:57 +0530

    Update config

--- config.js
+++ config.js
@@ -2,34 +2,14 @@ module.exports = {
   dates: [new Date()],
   locations: [
     {
-      type: "district",
-      districtId: 304,
-      title: "Pauline chechy - Kottayam",
+      type: "pincode",
+      pincode: 695004,
+      title: "Pattom, Trivandrum",
     },
     {
       type: "district",
       districtId: 296,
       title: "Trivandrum",
-    },
-    {
-      type: "district",
-      districtId: 380,
-      title: "Shraddha - Nagpur",
-    },
-    {
-      type: "district",
-      districtId: 154,
-      title: "Shraddha - Ahmedabad",
-    },
-    {
-      type: "district",
-      districtId: 770,
-      title: "Shraddha - Ahmedabad",
-    },
-    {
-      type: "pincode",
-      pincode: 382475,
-      title: "Shraddha - Hansol (382475)",
     },
   ],
 };
